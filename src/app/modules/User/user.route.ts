135767a05b1329182f16f0a1ffd556aae457dead
mehnaz <mehnazchowdhury613@gmail.com>
2024-08-30 12:53:46 +0600

    updated

--- src/app/modules/User/user.route.ts
+++ src/app/modules/User/user.route.ts
@@ -7,29 +7,17 @@ import { UserValidation } from "./user.validation";
 
 const router = express.Router();
 
-router.get(
-  "/",
-  auth(USER_ROLE.superAdmin, USER_ROLE.admin),
-  UserControllers.getAllUsers
-);
+router.get("/", auth(USER_ROLE.admin), UserControllers.getAllUsers);
 
-router.get(
-  "/:email",
-  auth(USER_ROLE.superAdmin, USER_ROLE.admin),
-  UserControllers.getSingleUser
-);
+router.get("/:email", auth(USER_ROLE.admin), UserControllers.getSingleUser);
 
 router.patch(
   "/:id",
-  auth(USER_ROLE.superAdmin, USER_ROLE.admin),
+  auth(USER_ROLE.admin),
   validateRequest(UserValidation.updateUserValidationSchema),
   UserControllers.updateUser
 );
 
-router.delete(
-  "/:id",
-  auth(USER_ROLE.superAdmin, USER_ROLE.admin),
-  UserControllers.deleteUser
-);
+router.delete("/:id", auth(USER_ROLE.admin), UserControllers.deleteUser);
 
 export const userRoutes = router;
